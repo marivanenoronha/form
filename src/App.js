7140fdd238186f9597b009339d4ea2aa3e8f01d9
Marivanenoronha <noronhamarivane4@gmail.com>
2023-02-15 16:21:15 -0300

    creates useState and onChange

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import { useState } from "react";
 import "./styles.css";
 
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+
+
+  function sendEmail(e){
+    e.preventDefault();
+
+    if(name === "" || email === "" || message === ""){
+      alert("preencha todos os campos");
+      return;
+    }
+    alert("TESTE")
+  }
+
+
   return (
     <div className="container">
       <h1 className='title'>Contato</h1>
-      <div className='containerInput'>
-        <input className="input" type="text" placeholder='Digite seu nome...'/>
-        <input className="input" type="text" placeholder='Digite seu e-mail...'/>
-        <textarea className="input" type="text" placeholder='Digite uma mensagem...'/>
-        <button className="button" type="submit">Enviar</button>
-      </div>
+      <form className="form" onSubmit={sendEmail}>
+        <div className='containerInput'> 
+            <input className="input" type="text" placeholder='Digite seu nome...'
+              onChange={(e) => setName(e.target.value)} value={name} />
+
+            <input className="input" type="text" placeholder='Digite seu email...'
+              onChange={(e) => setEmail(e.target.value)} value={email}
+              />
+
+            <textarea className="input" type="text" placeholder='Digite uma mensagem...'
+              onChange={(e) => setMessage(e.target.value)} value={message}
+            />
+
+            <button className="button" type="submit">Enviar</button>
+        </div>
+      </form>
     </div>
+
   );
+         
 }
 
 export default App;
